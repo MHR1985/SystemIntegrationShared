f1d52b77dfb56ab0dba55ea0aad6699d4e9a633e
Kenneth L. Hansen <klh@minuba.dk>
2020-09-11 18:50:39 +0200

    added database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customer (accnum, name, amount) VALUES
+INSERT INTO Customer (id, name, amount) VALUES
 (5555, 'Slim Shady', 500),
 (4444, 'Steve Bobs', 5000),
 (6666, 'Jar Jar', 500000);
