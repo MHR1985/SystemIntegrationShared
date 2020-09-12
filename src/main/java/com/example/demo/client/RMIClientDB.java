37b1193ea01314ea3fa7621ada80fb8af3e29652
Simon Schønberg Bojesen <cph-sb339@cphbusiness.dk>
2020-09-12 20:27:22 +0200

    Case 2 and 6 done

--- src/main/java/com/example/demo/client/RMIClientDB.java
+++ src/main/java/com/example/demo/client/RMIClientDB.java
@@ -18,10 +18,12 @@ public class RMIClientDB {
         System.out.println("Press 2 to create Customers");
         System.out.println("Press 3 to get all Customers");
         System.out.println("Press 4 to transfer money between customers");
-        System.out.println("Press 5 to quit");
+        System.out.println("Press 5 to get a customer");
+        System.out.println("Press 6 to edit a customer");
+        System.out.println("Press 7 to quit");
     }
 
-    public static void main(String args[]) throws RemoteException, MalformedURLException, NotBoundException {
+    public static void main(String args[]) throws Exception {
 
         String remoteEngine = "rmi://localhost/BankServices";
 
@@ -42,9 +44,12 @@ public class RMIClientDB {
                     break;
                 }
                 case (2): {
-                    System.out.println("Enter customer name:");
-                    String name = scan.next();
-                    Customer create = bankInterface.createCustomer(name, 50000000.0);
+                    System.out.println("Enter customers first name:");
+                    String firstName = scan.next();
+                    System.out.println("Enter customers last name:");
+                    String lastName = scan.next();
+                    String fullName = firstName + " " + lastName;
+                    Customer create = bankInterface.createCustomer(fullName, 50000000.0);
                     System.out.println("New customer was created under the name: " + create.getName());
                     System.out.println("Account contains " + create.getAmount());
                     break;
@@ -72,10 +77,33 @@ public class RMIClientDB {
                         ex.printStackTrace();
                     }
                     break;
-
                 }
                 case (5): {
+                    System.out.println("please type the id of the customer");
+                    long id = scan.nextLong();
+                    try {
+                        Customer c = bankInterface.getCustomer(id);
+                        System.out.println("Id: " + c.getId() + " Name: " + c.getName() + " Balance: " + c.getAmount());
+                    } catch (Exception ex){
+                        System.out.println(ex.getMessage());
+                    }
+                    break;
+                }
+                case (6): {
+                    System.out.println("Enter id of customer you want to edit:");
+                    long id = scan.nextLong();
+                    System.out.println("Enter customers first name:");
+                    String firstName = scan.next();
+                    System.out.println("Enter customers last name:");
+                    String lastName = scan.next();
+                    String fullName = firstName + " " + lastName;
+                    Customer edit = bankInterface.editCustomer(id, fullName);
+                    System.out.println("Customer name was changed to: " + edit.getName());
+                    break;
+                }
+                case (7): {
                     run = false;
+                    break;
                 }
             }
         }
